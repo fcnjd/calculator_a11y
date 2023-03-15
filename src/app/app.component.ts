60f20b5e4aa3262ac14d14002eff539a1c1f199f
Julian Dreykorn <dreykorn.julian@gmail.com>
2023-03-15 01:40:06 +0100

    Changes JavaScript to TypeScript code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,23 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { evaluate } from 'mathjs';
-import { FocusMonitor } from '@angular/cdk/a11y';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'calculator_a11y';
-  constructor(private focusMonitor: FocusMonitor) {
-  }
   rechnung = '';
   ergebnis = 'Hier kommt das Ergebnis rein';
-  @ViewChild(ElementRef, { static: false })
-  resultInputRef: ElementRef;
-  /*@ViewChild('resultInput', { static: false }) set content(content: ElementRef) {
-    this.resultInput = content;
-  }*/
+  @ViewChild('resultInput')
+  ergebnisElement: ElementRef;
   berechnen() {
     console.log('berechnen');
     this.ergebnis = evaluate(this.rechnung);
-    //this.focusMonitor.focusVia(this.resultInputRef, 'program');
-    //this.resultInputRef.nativeElement.focus();
-    //setTimeout(() => this.resultInputRef.nativeElement.focus(), 0);
-    //document.getElementById('resultInput').focus();
-    var element = document.getElementById('ergebnis');
-    if (element != null) {
+    if (this.ergebnisElement != null) {
       console.log('focus');
-      element.focus();
+      this.ergebnisElement.nativeElement.focus();
     }
   }
 }
