8de66a52afa7fafb20475f6a1af7f3be91ebcb1e
Julian Dreykorn <dreykorn.julian@gmail.com>
2023-11-23 14:10:03 +0100

    Tries to fix Electron

--- main.ts
+++ main.ts
@@ -1,34 +1,44 @@
-import { app, BrowserWindow } from 'electron';
-
-let win: BrowserWindow | null;
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
 
 function createWindow() {
-  win = new BrowserWindow({
-    width: 800,
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
+      preload: path.join(__dirname, "preload.js"),
+    },
+    width: 800,
   });
 
-  win.loadFile('dist/calculator_a11y/index.html');
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, "dist/calculator_a11y/index.html"));
 
-  win.on('closed', () => {
-    win = null;
-  });
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 }
 
-app.on('ready', createWindow);
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
-  if (win === null) {
-    createWindow();
-  }
-});
-
+// In this file you can include the rest of your app"s specific main process
+// code. You can also put them in separate files and require them here.
